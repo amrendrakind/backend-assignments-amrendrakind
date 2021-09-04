abfe0a44c0fc5c9e135fcd0e038657a1797b4d4a
amrendrakind <vision2010@gmail.com>
2021-09-05 03:02:26 +0530

    3rd backend assignment

---
+++ 3rd Assginment/dbConfig.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+dotenv.config()
+
+const MONGODB_URI = process.env.DATABASE_URL 
+
+const connectDB = async () => {
+    mongoose.connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+
+    mongoose.connection.on("connected", () => {
+        console.log("MongoDB Atlas Server is connected");
+    });
+}
+export default connectDB;
