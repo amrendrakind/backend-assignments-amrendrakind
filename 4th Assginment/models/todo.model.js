784e096db7a277182709a3a45b055ebb2c85469f
amrendrakind <vision2010@gmail.com>
2021-09-06 17:24:23 +0530

    4th backend assignment

---
+++ 4th Assginment/models/todo.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+const TodoSchema = mongoose.Schema(
+  {
+    userName: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+    },
+    todoTitle: {
+      type: String,
+    },
+    isComplete:{
+      type : Boolean,
+      default : false
+    },
+    status: { 
+      type: String,
+      default: "Just Started"
+    },
+    category: { 
+      type: String,
+      enum :['work','hobby','task','Work','Hobby','Task'],
+      default : 'hobby'
+
+    },
+  createdAt :{
+      type : Date,
+      default : Date.now
+  },
+  updatedAt: {
+      type: Date,
+      default: Date.now
+  },
+
+  },
+  { timestamps: true }
+);
+export default mongoose.model("TodoModel", TodoSchema);
---
+++ 4th Assginment/models/user.model.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username : {
+        type: String,
+        required:true,
+        unique:true,
+        trim:true
+    },
+    email : {
+        type: String,
+        required:true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,'Valid email only!']
+    },
+    phone : {
+        type: String,
+        required : true,
+        validator: function(v) {
+            return /\d{10}/.test(v);
+          },
+          message: props => `${props.value} is not a valid phone number!`
+    },
+    password : {
+        type : String,
+        required : [true, 'Password can not be blank!'],
+        minlength : 6,
+    },
+    role : {
+        type : String,
+        enum :['admin','appuser'],
+        default : 'user'
+    },
+    accessToken: {
+        type: String,
+    },
+    createdAt :{
+        type : Date,
+        default : Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    },
+    todo :[{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "TodoModel"
+    }],
+
+},
+{timeStamps:true});
+
+export default mongoose.model("User", userSchema);
