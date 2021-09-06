0b7e26d7061b575a6bcbc7c9b790aab2c0586383
amrendrakind <vision2010@gmail.com>
2021-09-06 16:50:19 +0530

    3rd backend assignment

--- 3rd Assginment/dbConfig.js
+++ 3rd Assginment/dbConfig.js
@@ -1,15 +1,14 @@
 import dotenv from "dotenv"
 import mongoose from "mongoose";
 dotenv.config()
-//DATABASE_URL = `mongodb+srv://favyt:mvKm7dKjIWcnxynd@cluster-favyt.nlf6u.mongodb.net/ToDoList?retryWrites=true&w=majority`
 
 const MONGODB_URI = process.env.DATABASE_URL 
 
 const connectDB = async () => {
     mongoose.connect(MONGODB_URI, {
         useNewUrlParser: true,
-//        useCreateIndex: true,
         useUnifiedTopology: true,
+
     });
 
     mongoose.connection.on("connected", () => {
