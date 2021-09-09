a58b6d98f7098603c7a5b44652679aaef4f663d3
amrendrakind <vision2010@gmail.com>
2021-09-09 22:02:23 +0530

    5th Assignment - Pagination with mongoose

--- 5th Assignment/controllers/todo.pagination.controller.js
+++ 5th Assignment/controllers/todo.pagination.controller.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const mongoosePaginate = require('mongoose-paginate-v2');
+const { findOne } = require("../models/todo.model.js");
 
-const TodoModel = require('../models/todo.pagination.model.js')
+const TodoModel = require('../models/todo.model.js')
 
 const getPagination = (page, size) => {
     const limit = size ? +size : 3;
@@ -26,20 +27,15 @@ module.exports.createTodo = async (req, res) => {
 module.exports.allTodo = async (req, res) => {
 
     const { page, size, title } = req.query;
-  
-    var condition = title
-        ? { title: { $regex: new RegExp(title), $options: "i" } }
-        : {};
+    
+    var condition = title 
+        ? {  todoTitle : { $regex: new RegExp(title), $options: "i" } } : {};
 
     const { limit, offset } = getPagination(page, size);
 
     TodoModel.paginate(condition, { offset, limit })
 
-    // TodoModel.find()
-        // .select("userName todoTitle status category")
-        // .exec()
         .then((data) => {
-            // res.status(200).json(data);
             res.send({
                 totalItems: data.totalDocs,
                 Todo: data.docs,
@@ -59,6 +55,7 @@ module.exports.allTodo = async (req, res) => {
 
 //search todo by id
 module.exports.todoByid = async (req, res) => {
+
     TodoModel.findById(req.params.id)
         .then((doc) => {
             if (!doc) {
@@ -75,125 +72,135 @@ module.exports.todoByid = async (req, res) => {
 
 module.exports.todoByUser = async (req, res) => {
 
-    var query = req.params.query;
-    TodoModel.find({
-        'userName': query
-    }, function (err, result) {
-        if (err) throw err;
-        if (result) {
-            res.json(result)
-        } else {
-            res.send(JSON.stringify({
-                error: 'Error'
-            }))
-        }
-    })
-};
+    const { page, size, name } = req.query;
+    const { limit, offset } = getPagination(page, size);
 
+    TodoModel.paginate({userName : name}, { offset, limit })
 
-//updating to do
-module.exports.updateTodo = async (req, res) => {
-    const { userName, status, todoTitle, category } = req.body;
-    const userExist = await TodoModel.findById(req.params.id);
-    if (userExist) {
-        const key = userExist._id;
-        const updateuser = new TodoModel({ status, category });
-        TodoModel.findByIdAndUpdate(key, {
-            userName: req.body.userName,
-            status: req.body.status,
-            todoTitle: req.body.todoTitle,
-            category: req.body.category,
-        }).then(() => {
-            res.status(200).json({ message: "Todo updated.." });
+        .then((data) => {
+            res.send({
+                totalItems: data.totalDocs,
+                Todo: data.docs,
+                totalPages: data.totalPages,
+                currentPage: data.page - 1,
+            })
+
+        })
+        .catch((er) => {
+            res.status(500).json({
+                message: "Please enter correct name",
+            });
         });
-    }
-};
-//Delete todos by id
-module.exports.deleteTodo = async (req, res) => {
-    const { id } = req.params;
-    const userExist = await TodoModel.findById(req.params.id);
-    //console.log(TodoModel.count())
 
-    if (!userExist)
-        return res.status(404).send(`No Todo with id: ${id}`);
-
-    await TodoModel.findByIdAndRemove(id);
-
-    res.json({ message: "Todo deleted successfully." });
-};
-
-
-//Delete all todos
-module.exports.deleteAllTodo = async (req, res) => {
-    await TodoModel.deleteMany({});
-    res.json({ message: "All Todos deleted successfully." });
-    
 };
 
 // fetch by category (all data which has category)
 module.exports.category = async (req, res) => {
 
-    TodoModel.find({ category: { $exists: true, $ne: null } })
-        .then((doc) => {
-            if (!doc) {
-                return res.status(404).json("Todo is not available");
-            }
-            return res.status(200).json(doc);
+    const { page, size, name } = req.query;
+    const { limit, offset } = getPagination(page, size);
+
+    TodoModel.paginate({category : { $exists: true, $ne: null }}, { offset, limit })
+//TodoModel.paginate({category : name}, { offset, limit })
+        .then((data) => {
+            res.send({
+                totalItems: data.totalDocs,
+                Todo: data.docs,
+                totalPages: data.totalPages,
+                currentPage: data.page - 1,
+            })
+
         })
-        .catch((err) => {
-            res.send({ message: err.message });
+        .catch((er) => {
+            res.status(500).json({
+                message: "Some error occurred while retrieving category." || er.message
+          
+            });
         });
+
+
+    // TodoModel.find({ category: { $exists: true, $ne: null } })
+    //     .then((doc) => {
+    //         if (!doc) {
+    //             return res.status(404).json("Todo is not available");
+    //         }
+    //         return res.status(200).json(doc);
+    //     })
+    //     .catch((err) => {
+    //         res.send({ message: err.message });
+    //     });
 };
 
 // fetch all data by category type
 module.exports.categoryName = async (req, res) => {
+    const { page, size, name } = req.query;
+    const { limit, offset } = getPagination(page, size);
 
-    var query = req.params.query;
-    TodoModel.find({
-        'category': query
-    }, function (err, result) {
-        if (err) throw err;
-        if (result) {
-            res.json(result)
-        } else {
-            res.send(JSON.stringify({
-                error: 'Error'
-            }))
-        }
-    })
+    TodoModel.paginate({category : name}, { offset, limit })
+        .then((data) => {
+            res.send({
+                totalItems: data.totalDocs,
+                Todo: data.docs,
+                totalPages: data.totalPages,
+                currentPage: data.page - 1,
+            })
+
+        })
+        .catch((er) => {
+            res.status(500).json({
+                message: "Some error occurred while retrieving category." || er.message
+          
+            });
+        });
 };
 
 // fetch by todo title (all data which has title)
 module.exports.todoTitle = async (req, res) => {
 
-    TodoModel.find({ todoTitle: { $exists: true, $ne: null } })
-        .sort( {createdAt :1})                  //Sort data by creation time stamp 
-        .then((doc) => {
-            if (!doc) {
-                return res.status(404).json("Todo is not available");
-            }
-            return res.status(200).json(doc);
+    const { page, size } = req.query;
+    const { limit, offset } = getPagination(page, size);
+
+    TodoModel.paginate({todoTitle : { $exists: true, $ne: null }}, { offset, limit })
+        .then((data) => {
+            res.send({
+                totalItems: data.totalDocs,
+                Todo: data.docs,
+                totalPages: data.totalPages,
+                currentPage: data.page - 1,
+            })
+
         })
-        .catch((err) => {
-            res.send({ message: err.message });
+        .catch((er) => {
+            res.status(500).json({
+                message: "Some error occurred while retrieving Title." || er.message
+          
+            });
         });
 };
 
 // fetch all data by todo title type
 module.exports.todoTitleName = async (req, res) => {
 
-    var query = req.params.query;
-    TodoModel.find({
-        'todoTitle': query
-    }, function (err, result) {
-        if (err) throw err;
-        if (result) {
-            res.json(result)
-        } else {
-            res.send(JSON.stringify({
-                error: 'Error'
-            }))
-        }
-    })
+    const { page, size, title } = req.query;
+    const { limit, offset } = getPagination(page, size);
+
+    TodoModel.paginate({todoTitle : title}, { offset, limit })
+        .then((data) => {
+            res.send({
+                totalItems: data.totalDocs,
+                Todo: data.docs,
+                totalPages: data.totalPages,
+                currentPage: data.page - 1,
+            })
+
+        })
+        .catch((er) => {
+            res.status(500).json({
+                message: "Some error occurred while retrieving category." || er.message
+          
+            });
+        });
+
+
 };
 
--- 5th Assignment/routes/todo.pagination.route.js
+++ 5th Assignment/routes/todo.pagination.route.js
@@ -5,9 +5,6 @@ const {  createTodo,
   allTodo,
   todoByid,
   todoByUser,
-  updateTodo,
-  deleteTodo,
-  deleteAllTodo,
   category,
   categoryName,
   todoTitle,
@@ -15,15 +12,13 @@ const {  createTodo,
 } = require('../controllers/todo.pagination.controller.js')
 
 router.post("/add", createTodo);                           // Add todos
-
-router.get("/", allTodo);                          // Get all todos with Pagination
-
-router.get("/todo/:id", todoByid);                         // get todos by id
-router.get("/user/:query", todoByUser);                    // get todos by user
-router.get("/category", category);                         // get todos by all todo Category 
-router.get("/category/:query", categoryName);              // get todos by Category type
-router.get("/title", todoTitle);                           // get todos by all todo title
-router.get("/title/:query", todoTitleName);                // get todos by todo title name
-
+//Pagination in all GET
+router.get("/", allTodo);                                 // Get all todos with Pagination localhost:4000/todoreport?page=1&size=5
+router.get("/todoid/:id", todoByid);               // get todos by id // no need to paginate as it will alwayts produce one record
+router.get("/user", todoByUser);                    // get todos by user  // localhost:4000/todoreport/user?name=6134842ba7e60af5a3f35bf8
+router.get("/category", category);                  // get todos by all todo Category localhost:4000/todoreport/category
+router.get("/categoryname", categoryName);          // get todos by Category type  localhost:4000/todoreport/categoryname?name=hobby
+router.get("/title", todoTitle);                    // get todos by all todo title  localhost:4000/todoreport/title
+router.get("/titlename", todoTitleName);            // get todos by todo title name  localhost:4000/todoreport/titlename?title=Playing
 
 module.exports = router
